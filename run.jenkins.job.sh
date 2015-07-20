f9f28f68e4b4ca43574bbfb93592cfe2799f2805
nickboldt <nickboldt@gmail.com>
2015-07-20 11:14:27 -0400

    JBIDE-20312 better error reporting when JBDS_INSTALLER_NIGHTLY_FOLDER not defined
    JBIDE-20312 check for an installer jar in the default/backup JBDS_INSTALLER_NIGHTLY_FOLDER

--- run.jenkins.job.sh
+++ run.jenkins.job.sh
@@ -74,7 +74,14 @@ fi
 if [[ ! ${JBDS_INSTALLER_NIGHTLY_FOLDER} ]]; then
   # Folder from which to install the latest nightly JBDS build, and run the version watch comparing this latest against
   # the baseline JBDS_INSTALLERS. This will always overwrite if the version has changed since last time.
-  JBDS_INSTALLER_NIGHTLY_FOLDER=/qa/services/http/binaries/RHDS/9.0/snapshots/builds/${UPSTREAM_JOB}/installer/
+  if [[ -f $(find /qa/services/http/binaries/RHDS/9.0/snapshots/builds/${UPSTREAM_JOB}/latest/all/ -maxdepth 1 -type f -name "*installer*.jar" | head -1) ]]; then # JBDS 9
+    JBDS_INSTALLER_NIGHTLY_FOLDER=/qa/services/http/binaries/RHDS/9.0/snapshots/builds/${UPSTREAM_JOB}/latest/all/
+  elif [[ -f $(find /qa/services/http/binaries/RHDS/9.0/snapshots/builds/${UPSTREAM_JOB}/installer/ -maxdepth 1 -type f -name "*installer*.jar" | head -1) ]]; then # JBDS 8 and earlier
+    JBDS_INSTALLER_NIGHTLY_FOLDER=/qa/services/http/binaries/RHDS/builds/staging/${UPSTREAM_JOB}/installer/
+  fi
+fi
+if [[ ! ${JBDS_INSTALLER_NIGHTLY_FOLDER} ]]; then
+  echo "[ERROR] No JBDS nightly folder defined in JBDS_INSTALLER_NIGHTLY_FOLDER = ${JBDS_INSTALLER_NIGHTLY_FOLDER}"
 fi
 
 # define globals in case they were overridden above
